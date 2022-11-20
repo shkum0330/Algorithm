5483ea43e15c7040cd3a1f4b66e656746f23c4d8
shkum0330 <shkum0330@gmail.com>
2022-11-20 18:13:09 +0900

    .

--- Programmers/네트워크.cpp
+++ Programmers/네트워크.cpp
@@ -1,30 +1,44 @@
-#include <string>
 #include <vector>
 #include <iostream>
-
+#include <queue>
 using namespace std;
-int e[201][201];
-bool visited[201];
+int movex[4]={0,-1,0,1};
+int movey[4]={-1,0,1,0};
+int v[101][101];
+bool visited[101][101];
+int dist[101][101];
 
-void dfs(int v,int n){
-    visited[v]=true;
-    for(int i=1;i<=n;i++){
-        if(!visited[i]&&e[v][i]) dfs(i,n);
-    }
-}
-int solution(int n, vector<vector<int>> computers) {
+
+int solution(vector<vector<int> > maps)
+{
     int answer = 0;
-    for(int i=0;i<computers.size();i++){
-        for(int j=0;j<computers[i].size();j++){
-            if(computers[i][j]) e[i+1][j+1]=1;
+    int n=maps.size();
+    int m=maps[0].size();
+    
+    for(int i=0;i<maps.size();i++){
+        for(int j=0;j<maps[0].size();j++) {
+            v[i+1][j+1]=maps[i][j];
         }
     }
-    for(int i=1;i<=n;i++){
-        
-        if(!visited[i]){
-            answer++;
-            dfs(i,n);
+    dist[1][1]=1;
+    queue<pair<int,int>> q;
+    q.push({1,1});
+    while(!q.empty()){
+        int y=q.front().first;
+        int x=q.front().second;
+        q.pop();
+        for(int i=0;i<4;i++){
+            int nx=x+movex[i];
+            int ny=y+movey[i];
+            if(ny>n || ny<=0 || nx>m || nx<=0) continue;
+            if(!visited[ny][nx] && v[ny][nx]){
+                q.push({ny,nx});
+                visited[ny][nx]=true;
+                dist[ny][nx]=dist[y][x]+1;
+            }
         }
     }
-    return answer;
+    if(dist[n][m]==0) return -1;
+    
+    return dist[n][m];
 }
