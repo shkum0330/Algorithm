120495792280dcfdecae03b0b3d6af5b1ccef8f8
shkum0330 <shkum0330@gmail.com>
2023-12-08 17:30:25 +0900

    fix

--- Programmers/동굴 탐험.cpp
+++ Programmers/동굴 탐험.cpp
@@ -1,31 +1,71 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <stack>
+#include <map>
+#include <queue>
+#include <set>
 using namespace std;
 
-int solution(vector<vector<int>> board, vector<int> moves) {
-    int answer = 0;
-    stack<int> st;
-    
-    for(int i=0;i<moves.size();i++){
-        for(int j=0;j<board.size();j++){
-            if(board[j][moves[i]-1]){
-        
-                if(st.empty() || st.top()!=board[j][moves[i]-1]){
-                    st.push(board[j][moves[i]-1]);
-                 
-                    board[j][moves[i]-1]=0;
-                    break;
+bool visited[200001];
+vector<int> e[200001];
+map<int,int> m;
+set<int> res,s;
+
+void bfs(int v){
+    queue<int> q;
+    q.push(v);
+    visited[v]=true;
+    res.insert(v);
+    while(!q.empty()){
+        int k=q.front();
+        q.pop();
+        for(auto& i:e[k]){
+            if(!visited[i]){
+                int prev=m[i];
+                if(!visited[prev]){
+                    s.insert(i);
                 }
-                if(st.top()==board[j][moves[i]-1]){
-                    cout<<st.top()<<' ';
-                    answer += 2;
-                    st.pop();
-                    board[j][moves[i]-1]=0;
-                    break;
+                else{
+                    visited[i]=true;
+                    res.insert(i);
+                    q.push(i);
                 }
             }
+        }
+    }
+}
+
+bool solution(int n, vector<vector<int>> path, vector<vector<int>> order) {
+    bool answer = true;
+    for(int i=0;i<path.size();i++){
+        e[path[i][0]].push_back(path[i][1]);
+        e[path[i][1]].push_back(path[i][0]);
+    }
+    for(int i=0;i<order.size();i++){
+        m[order[i][1]]=order[i][0];
+    }
+
+    bfs(0);
+    res.insert(0);
+
+    while(true){
+        int tmp_size=res.size();
+
+        for(auto& k:s){
+            if(!visited[k] && visited[m[k]]) {
+                bfs(k);
+                s.erase(k);
+            }
+        }
+
+        // bfs를 돌렸는데도 변화가 없으면 false
+        if(tmp_size==res.size()){
+            answer=false;
+            break;
+        }
+        // 모두 방문했으면 true
+        if(res.size()==n){
+            answer=true;
+            break;
         }
     }
 
